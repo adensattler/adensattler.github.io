e2d6788fb02456dcc063ba741139cfc90b47a9b2
Aden Sattler <adensattler@gmail.com>
2024-07-05 17:01:38 -0600

    header rework

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Header.css';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
-      <div className="titles">
-        <h1 className="name">
-          <Link to="/">Aden Sattler</Link>
-        </h1>
+      <div className="name">
+        <Link to="/">Aden Sattler</Link>
       </div>
       <div className="nav-container">
         <Link to="/">Work</Link>
